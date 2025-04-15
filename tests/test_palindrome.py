b4cf77dd7a271389658cbb8a8d57502eda502bc6
Santiago Ortega <sa.ortega@alumno.um.edu.ar>
2025-04-14 22:16:36 -0300

    palabras no palindromes

---
+++ tests/test_palindrome.py
@@ -0,0 +1,48 @@
+from src.palindrome import is_palindrome
+import unittest
+
+
+class TestPalindrome(unittest.TestCase):
+
+    def test_a(self):
+        resultado = is_palindrome("a")
+        self.assertEqual(resultado, True)
+
+    def test_aa(self):
+        resultado = is_palindrome ("aa")
+        self.assertEqual(resultado,(True))
+  
+    def test_as(self):
+        resultado = is_palindrome ("as")
+        self.assertEqual(resultado, False)
+
+    def test_aca(self):
+        resultado = is_palindrome("ana")
+        self.assertEqual(resultado, True)
+
+    def test_apta(self):
+        resultado = is_palindrome("apta")
+        self.assertEqual(resultado, False)
+
+    def test_neuquen(self):
+        resultado = is_palindrome('neuquen')
+        self.assertEqual(resultado, True)
+
+    def test_son_robos_o_sobornos(self):
+        resultado = is_palindrome("son robos o sobornos")
+        self.assertEqual(resultado, True)
+
+    def test_anita_lava_la_tina(self):
+        resultado = is_palindrome("anita lava la tina")
+        self.assertEqual(resultado, True) 
+
+    def test_amo_la_pacifica_paloma(self):  
+        resultado = is_palindrome("amo la pacifica paloma")
+        self.assertEqual(resultado, True)
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+
+
+     
