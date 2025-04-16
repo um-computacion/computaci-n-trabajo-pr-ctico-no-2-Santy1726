af06d073fd61880ebf08e5e305a4357bd5308895
Santiago Ortega <sa.ortega@alumno.um.edu.ar>
2025-04-15 23:50:55 -0300

    Archivo limpieza de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,12 +1,5 @@
-def is_palindrome(mystring):
+def limpiar_texto(mystring):
     mystring = ''.join(c.lower() for c in mystring if c.isalnum())
-    print(f"Cadena procesada: {mystring}") 
-    for indice in range(0, len(mystring)):
-        print(f"{mystring[indice]} --> {mystring[-(indice + 1)]}")  
-        if mystring[indice] != mystring[-(indice + 1)]:
-            print("no es un palíndromo")
-            return False
-    return True
-
+    
 
 
